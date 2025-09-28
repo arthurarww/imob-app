3b2d6dd99a7cee6b4b321720c52c14c6e4fd91f4
Arthur Warmling <arthurwarmling1@gmail.com>
2025-09-27 23:40:24 -0300

    Add Docker support with Dockerfile and docker-compose configuration; update .gitignore and enhance dialog component

---
+++ resources/js/pages/leads/dialog-store-lead/index.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
+import { PlusIcon } from 'lucide-react';
+
+export default function DialogStoreLead() {
+    return (
+        <Dialog>    
+            <DialogTrigger asChild>
+                <Button>
+                    <PlusIcon className="size-4" />
+                    Adicionar Lead
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogDescription>
+                        This action cannot be undone. This will permanently
+                        delete your account and remove your data from our
+                        servers.
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
+}
--- resources/js/pages/leads/index.tsx
+++ resources/js/pages/leads/index.tsx
@@ -4,6 +4,7 @@ import { BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import { DataTable } from './data-table';
 import { columns } from './data-table/columns';
+import DialogStoreLead from './dialog-store-lead';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -21,6 +22,9 @@ export default function LeadsIndex() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Leads" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                <div className="flex justify-end">
+                    <DialogStoreLead />
+                </div>
                 <DataTable columns={columns} data={[]} />
             </div>
         </AppLayout>
